b6dfabdcfd95bee92b5fc7c4b5cb593d7a093e6d
Solverj <solve.johnsen@gmail.com>
2014-12-01 09:08:06 +0100

    stuff works, testing git stuff

--- view3/view3.js
+++ view3/view3.js
@@ -9,15 +9,61 @@ angular.module('myApp.view3', ['ngRoute'])
         });
     }])
 
-    .controller('View3Ctrl', [function () {
+    .controller('View3Ctrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.send = function () {
+            if ($scope.subject && $scope.text && $scope.to) {
+                var message = {};
+                message.subject = $scope.subject;
+                message.text = $scope.text;
+                message.organisationUnits = [];
+                message.users = [];
+                message.userGroups = [];
+                console.log('inside if test');
 
+                //running through users to find the one we said "to"
+                $http.get(dhi + '/api/users')
+                .success(function (response) {
+                        var rsp = response;
+                        var i;
+                        console.log('inside getting test');
+                        for(i = 0; i < rsp.users.length; i++){
+                            if('John Traore' === rsp.users[i].name){
+                                console.log(rsp.users[i].id);
+                                message.users.push( { "id" : rsp.users[i].id } );
+                                break;
+                            }
+                        }
+                        //paste stuff
+                        var send = $http({
+                            method: 'post',
+                            url: dhi + '/api/messageConversations',
+                            contentType: 'application/json',
+                            data: JSON.stringify(message),
+                            async: false
+                        }).success(function () {
+                            console.log('well fk yeah?');
+                        }).error(function (data, status, headers, config) {
+                            console.log(data, status, headers, config);
+                        });
+
+
+
+
+
+                    });
+
+
+            }//if test
+            $scope.text ='';
+            $scope.subject='';
+            $scope.to='';
+
+        };
     }]);
 
 function getMe($scope, $http) {
-    console.log('k')
-    $http.get(dhi+'api/me')
+    $http.get(dhi + '/api/me')
         .success(function (response) {
-            $scope.email = response;
-            console.log('k')
+            $scope.me = response;
         });
 }
--- view4/view4.js
+++ view4/view4.js
@@ -1,20 +1,70 @@
 'use strict';
 
+
 angular.module('myApp.view4', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view4/:id', {
-    templateUrl: 'view4/view4.html',
-    controller: 'View4Ctrl'
-  });
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view4/:id', {
+            templateUrl: 'view4/view4.html',
+            controller: 'View4Ctrl'
+        });
 
-     // $locationProvider.html5Mode(true);
-}])
+        // $locationProvider.html5Mode(true);
+    }])
+    .controller('View4Ctrl', ['$http', '$scope', '$routeParams', function ($http, $scope, $routeParams) {
+        $scope.messageId = ($routeParams.id);
+        $http.get(dhi + '/api/me')
+            .success(function (response) {
+                $scope.me = response;
+            });
 
-.controller('View4Ctrl', [function($rootScope, $scope, $routeParams, $route) {
-    $rootScope.$on('$routeChangeSuccess', function ()  {
-        console.log($routeParams.id);
-      });
-}]);
+    }]);
+
+function messageController($scope, $http,$route) {
+
+    $scope.messagrURL = dhi + "/api/messageConversations/" + $scope.messageId;
+    $http({
+        method: 'get',
+        url: $scope.messagrURL+'/messages',
+        contentType: 'application/json',
+        async: false
+
+        }).success(function (result) {
+        $scope.replyText = "";
+        $scope.inboxList = [];
+        $scope.json = result;
+        // $scope.messageUserList = [];
+        $scope.messageThread = result.messages;
+        $scope.subject = result.subject;
+        $scope.people = result.userMessages;
+         for (var i = 0; i < result.messages.length; i++) {
+             console.log(result.messages[i]);
+         $scope.inboxList.push({"id": result.messages[i]});
+         }
+
+    });
+
+    $scope.reply = function () {
+        if($scope.text) {
+            var urlTo = dhi + '/api/messageConversations/' + $scope.messageId;
+            $http({
+                method: 'post',
+                url: urlTo,
+                contentType: 'application/json',
+                data: JSON.stringify($scope.text),
+                async: false
+            }).success(function () {
+                console.log('well fk yeah?');
+                $route.reload();
+            }).error(function (data, status, headers, config) {
+                console.log(data, status, headers, config);
+            });
+            $scope.text = '';
+
+        }
+
+    }
+}
+
 
 //http://inf5750-30.uio.no/api/messageConversations/id
