a8e9a812f492e73f0ac7832f681d799c8ee183c8
zOrky- <jacob.jedi@gmail.com>
2014-12-03 06:44:55 +0100

    Fixing
    See last commit. forgot to add view1 files.

--- view1/view1.js
+++ view1/view1.js
@@ -1,32 +1,66 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+var view1App = angular.module('myApp.view1', ['ngRoute', 'indexedDB_messageapp']);
 
-.config(['$routeProvider', function($routeProvider) {
+view1App.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
-}])
-
-.controller('View1Ctrl', [function() {
-
 }]);
 
+view1App.controller('View1Ctrl', function($scope, $window, dbService, $http) {
+   // $scope.messageList = retrieveUserConversations.getMessages;
+/*    $http.get(dhi + '/api/currentUser/inbox/messageConversations').success(function (response) {
+        addAllMessages(response);
+    });*/
+    var vm = this;
+    vm.todos = [];
+
+    vm.refreshList = function () {
+        dbService.getAllMessages().then(function (data) {
+            vm.todos = data;
+            $scope.messageList = vm.todos;
+        }, function (err) {
+            $window.alert(err);
+        });
+    };
+
+    function init() {
+        dbService.open().then(function () {
+            //if this is success full we should "refresh" our database with the server.
+            $http.get(dhi + '/api/currentUser/inbox/messageConversations').success(function (response) {
+
+                dbService.addAllMessages(response);
+            });
+            //And then retrieve from the database. see above.
+           vm.refreshList();
+        });
+    }
+    init();
+});
 
 
 
+/*
 function retrieveUserConversations($scope, $http) {
 
-    var test = $http.get(dhi + '/api/currentUser/inbox/messageConversations');
-
-    test.success(function (response) {
-        $scope.messageList = response;
-        addAllMessages(response);
-    });
-
-    test.error(function (response) {
-       $scope.messageList = getAllMessages();
+    $http.get(dhi + '/api/currentUser/inbox/messageConversations')
+        .success(function (response) {
+            addAllMessages(response);
+            $scope.messageList = response;
     });
 
 }
+//http://sravi-kiran.blogspot.no/2014/01/CreatingATodoListUsingIndexedDbAndAngularJs.html
+//http://stackoverflow.com/questions/15604196/promises-in-angularjs-and-where-to-use-them
+//http://markdalgleish.com/2013/06/using-promises-in-angularjs-views/
+
+function updateMessageList() {
+    var scope = angular.element($("#inbox")).scope();
+    console.log(getAllMessages());
+    scope.$apply(function() {
+        scope.messageList = getAllMessages();
+    })
+
+}*/
